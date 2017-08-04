f38a49ac5c065a455273d230163e6e4f8d671316
RavicharanN <ravicharan.vsp@gmail.com>
2017-08-04 15:44:41 +0530

    Fix the link bug

--- home/assets/js/modals.js
+++ home/assets/js/modals.js
@@ -6,19 +6,26 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// Get the container section 
+var sec = document.getElementById("sec");
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
+    sec.style.filter = "blur(2px)";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    sec.style.filter = "blur(0px)";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        sec.style.filter = "blur(0px)";
+
     }
 }
