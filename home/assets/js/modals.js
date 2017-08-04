1336ff3d946dfafdd3cbad53e30e7ee6f93e5c61
RavicharanN <ravicharan.vsp@gmail.com>
2017-08-04 18:57:55 +0530

    Change modal design

--- home/assets/js/modals.js
+++ home/assets/js/modals.js
@@ -1,27 +1,23 @@
+// Modal  : Gaganjeeth Reen
+
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// Get the container section 
 var sec = document.getElementById("sec");
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
     sec.style.filter = "blur(2px)";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
     sec.style.filter = "blur(0px)";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -29,3 +25,5 @@ window.onclick = function(event) {
 
     }
 }
+
+
